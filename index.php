566df75264f64069a5c1a168c24ef69e921fdbc6
Robert Pakosta <RobertPakosta@users.noreply.github.com>
2018-09-11 14:22:31 +0300

    lisatud esimene fail

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>Robert Pakosta,õppetöö</title>
+  </head>
+  <body>
+    <h1>Robert Pakosta</h1>
+	<p> <a href="http://www.tlu.ee">TLÜ</a> Õppetöö raames, pole mõtestatud ega väärtuslikku sisu.</p>
+	<p> minu sõber teeb ka <a href="../../~jaagala/"target="_blank">veebi</a>
+	<img src="../../~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_2.jpg" alt="TLÜ Terra õppehoone">
+  </body>
+</html>
